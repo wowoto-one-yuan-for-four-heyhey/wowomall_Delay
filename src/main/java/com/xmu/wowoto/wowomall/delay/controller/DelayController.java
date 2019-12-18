6d1333005c1a18e6934e3c12296eb7edd46352ce
medalwill <42199368+medalwill@users.noreply.github.com>
2019-12-18 11:14:05 +0800

    1

---
+++ src/main/java/com/xmu/wowoto/wowomall/delay/controller/DelayController.java
@@ -0,0 +1,43 @@
+package com.xmu.wowoto.wowomall.delay.controller;
+
+import com.xmu.wowoto.wowomall.delay.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author MedalWill
+ * @date 2019/12/17 21:16
+ */
+@RestController
+@RequestMapping("")
+public class DelayController {
+    @Autowired
+    RedisUtil redisUtil;
+
+    @RequestMapping("test")
+    public void test(){
+        Set<String> set=new HashSet<>();
+        set.add("100001");
+        set.add("100002");
+        set.add("100003");
+        set.add("100006");
+        System.out.println(redisUtil.hasKey("test"));
+        redisUtil.addToSet("test",set);
+        Set<String> ret=redisUtil.getSet("test");
+        for(String str : ret)
+        {System.out.println(str);}
+
+        redisUtil.addToSet("test",set);
+        redisUtil.addToSet("test1",set);
+        redisUtil.addToSet("test2",set);
+        redisUtil.setDelay("test1",60);
+        redisUtil.setDelay("test2",60*30);
+
+    }
+
+}
